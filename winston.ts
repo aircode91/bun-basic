22aa2f0657329bbf4900697d177c20e4915cfaad
AgusFirman <agus.firman@meteor.id>
2024-05-05 01:59:33 +0700

    add winston

---
+++ winston.ts
@@ -0,0 +1,21 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.Console(),
+        //
+        // - Write all logs with level `error` and below to `error.log`
+        // - Write all logs with level `info` and below to `combined.log`
+        //
+        // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        // new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+
+logger.info('Hello, World!');
+logger.warn('Hello, World!');
+logger.error('Hello, World!');
